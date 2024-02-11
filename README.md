767e20c22a05aabb4ec3e036c573c21a8ff9d74a
Pankaj Munde <pankaj.munde@shivrai.co.in>
2024-02-11 19:41:27 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ssdms
+## Website template that are intended to be used for SSDMS - श्री समर्थ धोंडुतात्या महाराज, श्रीक्षेत्र डोंगरशेळकी
+## All rights reserved by the Account Owner
---
+++ js/main.js
@@ -0,0 +1,131 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Fixed Navbar
+    $('.fixed-top').css('top', $('.top-bar').height());
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('.fixed-top').addClass('bg-dark').css('top', 0);
+        } else {
+            $('.fixed-top').removeClass('bg-dark').css('top', $('.top-bar').height());
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    // $(".header-carousel").owlCarousel({
+    //     autoplay: false,
+    //     smartSpeed: 1500,
+    //     loop: true,
+    //     nav: true,
+    //     dots: false,
+    //     items: 1,
+    //     navText : [
+    //         '<i class="bi bi-chevron-left"></i>',
+    //         '<i class="bi bi-chevron-right"></i>'
+    //     ]
+    // });
+
+    $(".header-carousel").owlCarousel({
+        autoplay: true, // Enable autoplay
+        autoplayTimeout: 3000, // Set autoplay interval to 3 seconds
+        smartSpeed: 1500,
+        loop: true,
+        nav: true,
+        dots: false,
+        items: 1,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+    
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    
+    
+
+    
+})(jQuery);
+
+// $('.owl-carousel').owlCarousel({
+//     items: 1, // Number of items to display
+//     loop: true, // Infinite loop
+//     autoplay: true, // Autoplay enabled
+//     autoplayTimeout: 30, // Autoplay interval in milliseconds
+//     autoplayHoverPause: true, // Pause autoplay on hover
+//     nav: true, // Show navigation buttons
+//     navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'], // Navigation button icons
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// });
